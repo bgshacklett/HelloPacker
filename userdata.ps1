e8fa4c1ac9b478bd45b4a945eab654a42d5f9f7e
Brian Shacklett <brian.shacklett@rackspace.com>
2017-08-14 01:56:24 +0000

    Update auth

--- userdata.ps1
+++ userdata.ps1
@@ -1,7 +1,11 @@
 <powershell>
+# Force all network connections to be private
+Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
+
+# Enable WinRM
 $WinRMSettings =
 @{
-  "UseSSL" = $false
+  "UseSSL" = $true
   "Force"  = $true
 }
 Set-WSManQuickConfig @WinRMSettings
