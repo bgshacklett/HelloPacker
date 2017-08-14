b86ce7f3254efa24ccf4eeb7ea851f6c175a11ca
Brian Shacklett <brian.shacklett@rackspace.com>
2017-08-14 02:53:30 +0000

    Change WinRM Config

--- userdata.ps1
+++ userdata.ps1
@@ -2,11 +2,29 @@
 # Force all network connections to be private
 Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
 
+
 # Enable WinRM
-$WinRMSettings =
+$WinRMBasicSettings =
 @{
   "UseSSL" = $false
   "Force"  = $true
 }
-Set-WSManQuickConfig @WinRMSettings
+
+$WinRMServiceAuthSettings =
+@{
+  ComputerName = "localhost"
+  resourceuri  = "winrm/config/service/auth"
+  valueset     = @{Basic="true"}
+}
+
+$WinRMServiceSettings =
+@{
+  ComputerName = "localhost"
+  resourceuri  = "winrm/config/service"
+  valueset     = @{AllowUnencrypted="true"}
+}
+
+Set-WSManQuickConfig @WinRMBasicSettings
+Set-WSManInstance @WinRMServiceAuthSettings
+Set-WSManInstance @WinRMSecuritySettings
 </powershell>
